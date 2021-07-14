cd9bdd1065576fb65e2cc2f1439a52c9c49d7887
stanzas <black_illis_dust@hotmail.com>
2021-07-14 14:49:53 +0200

    test

--- main.py
+++ main.py
@@ -243,7 +243,7 @@ def fDisplay():
       oDisplay.second = now.second
       oDisplay.blink = not oDisplay.blink
       if (oDisplay.blink == True):
-        tm.show("")
+        tm.numbers(oAlarm.hour, 0, colon=False)
       else:
         tm.numbers(oAlarm.hour, oAlarm.minute, colon=False)
 
